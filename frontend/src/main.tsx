4fb01b956db44413effd71c701b820e05c16791d
chenyuzhou <chenyuzhou@MacBook-Air.local>
2026-05-21 18:57:56 +0800

    Add production deployment bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { I18nProvider } from './i18n';
 
+const basename =
+  import.meta.env.VITE_FRONTEND_BASENAME ||
+  import.meta.env.BASE_URL.replace(/\/+$/, '') ||
+  undefined;
+
 ReactDOM.render(
   <React.StrictMode>
     <I18nProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/+$/, '') || undefined}>
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </I18nProvider>
